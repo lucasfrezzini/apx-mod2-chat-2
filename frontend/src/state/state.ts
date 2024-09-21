82d5caded22a4748cbd2f75bcef99ff0a1e771db
Lucas Frezzini <luquiitas.f@gmail.com>
2024-09-21 18:12:17 -0300

    Modeling state

--- frontend/src/state/state.ts
+++ frontend/src/state/state.ts
@@ -1,24 +1,46 @@
-interface State {
-  data: any;
-  listeners: any;
-  setUser: any;
-  setNewMessage: any;
-  getUser: any;
-  getMessages: any;
-}
-const state: State = {
+const state = {
   data: {
-    userAdmin: "",
+    user: {
+      admin: "",
+      email: "",
+      nombre: "",
+    },
+    roomIDshort: "",
+    roomIDlong: "",
+    mainRoom: true,
+    messages: [""],
+    listeners: [() => {}],
+  },
+  listeners: () => {
+    const currentState = state;
+    currentState.data.listeners.forEach((cb) => {
+      cb();
+    });
+  },
+  setListener: (cb: () => {}) => {
+    const currentState = state;
+    currentState.data.listeners.push(cb);
   },
   getUser: () => {
-    return state.data.userAdmin;
+    const currentState = state;
+    return currentState.data.user;
   },
-  listeners: () => {},
-  setUser: (username: string) => {
-    state.data.userAdmin = username;
+  setUser: (user: { admin: string; email: string; nombre: string }) => {
+    const currentState = state;
+    currentState.data.user = user;
+    currentState.listeners();
   },
-  setNewMessage: () => {},
-  getMessages: () => {},
+  setNewMessage: (message: any) => {
+    const currentState = state;
+    // ! REFACTOR: Array of never initialization (if statement)
+    if ((currentState.data.messages[0] = "")) currentState.data.messages = [];
+    currentState.data.messages.push(message);
+  },
+  getMessages: () => {
+    const currentState = state;
+    return currentState.data.messages;
+  },
+  // TODO: init() connect to rtdb
 };
 
 export default state;
